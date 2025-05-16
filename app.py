a62bcb219ad718928add2611d9f6b314225b5b32
sreenidhi-05 <sreenidhi0705@gmail.com>
2025-05-16 18:30:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -67,4 +67,5 @@ def index():
                            fertilizer_prediction=fertilizer_prediction)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    from waitress import serve
+    serve(app, host='0.0.0.0', port=5000)
