d764fbb4e39ee51d4a65bbbc3efc3227542c09c3
sreenidhi-05 <sreenidhi0705@gmail.com>
2025-05-16 18:25:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -67,4 +67,4 @@ def index():
                            fertilizer_prediction=fertilizer_prediction)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
